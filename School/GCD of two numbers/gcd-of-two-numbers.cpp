a33a9687636ff1c051309cbd02ceb6c41544a074
Mufaddalmohna <142883354+Mufaddal51@users.noreply.github.com>
2024-01-15 19:38:07 +0400

    Added Solution - GfG to GitHub

---
+++ School/GCD of two numbers/gcd-of-two-numbers.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+	public:
+    int gcd(int A, int B) 
+	{ 
+	    while(B){
+	        int rem=A%B;
+	        if(rem==0){
+	            return B;
+	        }
+	        else{
+	            A=B;
+	            B=rem;
+	            
+	        }
+	    }
+	    // code here
+	      
+	} 
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int A, B;
+        cin >> A >> B;
+        Solution ob;
+       	cout <<  ob.gcd(A, B) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
