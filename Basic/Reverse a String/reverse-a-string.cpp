b39c263d0356c87d9e6a15e2e8c654211060ce4a
Mufaddalmohna <142883354+Mufaddal51@users.noreply.github.com>
2024-01-16 14:47:02 +0400

    Updated Solution - GfG to GitHub

---
+++ Basic/Reverse a String/reverse-a-string.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    { int s=0;
+     int e=str.length()-1;
+    
+    while(s<=e){
+        char temp=str[s];
+        str[s]=str[e];
+        str[e]=temp;
+        s++;
+        e--;
+    }
+    return str;
+    
+        // Your code goes here
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+    	string s;
+    	cin >> s;
+    	Solution ob;
+    	cout << ob.reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+
+// } Driver Code Ends
