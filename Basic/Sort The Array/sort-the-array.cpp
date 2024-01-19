c918d4a34c204b27331b4b3d97251cf2fd7e3cbb
Mufaddalmohna <142883354+Mufaddal51@users.noreply.github.com>
2024-01-19 22:56:41 +0400

    Added Solution - GfG to GitHub

---
+++ Basic/Sort The Array/sort-the-array.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    vector<int> sortArr(vector<int>arr, int n){
+     
+     
+        
+     vector<int>ans;
+    sort(arr.begin(),arr.end());
+    for(int i=0;i<n;i++)
+    {
+        // cout<<arr[i]<
+        ans.push_back(arr[i]);
+    }
+    return ans;
+    }
+    
+};
+
+//{ Driver Code Starts.
+int main(){
+    
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int>s(n);
+        for(int i = 0; i < n; i++)
+            cin >> s[i];
+        Solution ob;
+        vector<int>v = ob.sortArr(s, n);
+        for(auto i : v)
+            cout << i << ' ';
+        cout << endl;
+    }
+return 0;
+}
+
+
+// } Driver Code Ends
